a075919883216f11ff743c62daf98334effae64f
Dawid Pereira <pereiradawid@outlook.com>
2024-09-05 18:20:11 +0200

    Changed view of cmp

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,9 +1,23 @@
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+
 return {
   "hrsh7th/nvim-cmp",
   event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
     "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-emoji",
     {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
@@ -22,33 +36,33 @@ return {
 
     local lspkind = require("lspkind")
     local kind_icons = {
-      Text = "",
-      Method = "",
-      Function = "",
+      Text = "",
+      Method = "󰆧",
+      Function = "󰊕",
       Constructor = "",
-      Field = "ﰠ",
+      Field = "󰇽",
       Variable = "",
-      Class = "",
+      Class = "󰠱",
       Interface = "",
       Module = "",
-      Property = "ﰠ",
-      Unit = "塞",
-      Value = "",
+      Property = "󰜢",
+      Unit = "",
+      Value = "󰎠",
       Enum = "",
-      Keyword = "",
+      Keyword = "󰌋",
       Snippet = "",
-      Color = "",
-      File = "",
-      Reference = "",
-      Folder = "",
+      Color = "󰏘",
+      File = "󰈙",
+      Reference = "",
+      Folder = "󰉋",
       EnumMember = "",
-      Constant = "",
-      Struct = "פּ",
+      Constant = "󰏿",
+      Struct = "",
       Event = "",
-      Operator = "",
-      TypeParameter = "",
+      Operator = "󰆕",
+      TypeParameter = "󰅲",
       Copilot = "",
-    } -- icons for completion itemsu
+    }
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
@@ -62,6 +76,16 @@ return {
           luasnip.lsp_expand(args.body)
         end,
       },
+      window = {
+        completion = {
+          winhighlight = "Normal:CmpPmenu",
+          border = border("CmpDocBorder"),
+        },
+        documentation = {
+          border = border("CmpDocBorder"),
+          winhighlight = "Normal:CmpDoc",
+        },
+      },
       mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
@@ -74,10 +98,11 @@ return {
       -- sources for autocompletion
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "copilot" },
         { name = "luasnip" }, -- snippets
+        { name = "copilot" },
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
+        { name = "emoji" }, -- file system paths
       }),
 
       formatting = {
